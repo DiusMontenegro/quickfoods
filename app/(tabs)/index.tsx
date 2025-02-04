f7f4b523d9fb71e85fbed7f992e62e6be5c333e3
Dius Montenegro <montenegrodiussantos1@gmail.com>
2025-02-04 17:16:21 +0800

    refactor: home screen for a cleaner look and render the new product component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import products from '~/assets/data/products';
+import Product from '~/components/shared/Product';
 
 const Home = () => {
-    const product = products[0];
-    const { image, name, price } = product;
-
     return (
-        <View className="rounded-2xl bg-white p-2">
-            <Image source={{ uri: image }} className="aspect-square w-full" />
-            <Text className="my-1 text-[18px] font-bold">{name}</Text>
-            <Text className="text-base font-semibold text-[#2f95dc]">${price}</Text>
+        <View>
+            <Product product={products[0]} />
         </View>
     );
 };
