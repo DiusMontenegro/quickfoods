aa1ac84b46761fe14d90cbbf69a3eb8d85a195fe
Dius Montenegro <montenegrodiussantos1@gmail.com>
2025-02-05 12:28:54 +0800

    refactor: Redirect index to the manu/index as the default home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,7 @@
 /* eslint-disable prettier/prettier */
+import { Redirect } from 'expo-router';
 import React from 'react';
-import { FlatList } from 'react-native';
 
-import products from '~/assets/data/products';
-import Product from '~/components/shared/Product';
-
-const Home = () => {
-    return (
-        <FlatList
-            data={products}
-            renderItem={({ item }) => <Product product={item} />}
-            numColumns={2}
-            contentContainerStyle={{ gap: 10, padding: 10 }}
-            columnWrapperStyle={{ gap: 10 }}
-        />
-    );
-};
-
-export default Home;
+export default function Menu() {
+    return <Redirect href={`${'/menu/'}` as any} />;
+}
