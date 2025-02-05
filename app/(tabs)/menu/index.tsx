a4da1ba119a42ad90c3b4ac88e22af6a78d897bd
Dius Montenegro <montenegrodiussantos1@gmail.com>
2025-02-05 12:30:02 +0800

    chore: Add new menu index page

---
+++ app/(tabs)/menu/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { FlatList } from 'react-native';
+
+import products from '~/assets/data/products';
+import Product from '~/components/shared/Product';
+
+export default function Menu() {
+    return (
+        <FlatList
+            data={products}
+            renderItem={({ item }) => <Product product={item} />}
+            numColumns={2}
+            contentContainerStyle={{ gap: 10, padding: 10 }}
+            columnWrapperStyle={{ gap: 10 }}
+        />
+    );
+}
